c0359e11690787e0dcc11ed7d8f96a82acc27025
Ashish Paudel <ashishthesatan@gmail.com>
2025-05-23 13:13:17 +0545

    added class-validator based validation

--- src/fiscal-year/entities/fiscal-year.entity.ts
+++ src/fiscal-year/entities/fiscal-year.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsDate, IsOptional, Matches } from 'class-validator';
 import { Vacancy } from '../../vacancy/entities/vacancy.entity';
 
 @Entity()
@@ -8,15 +9,22 @@ export class FiscalYear {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @ApiProperty({ description: 'Fiscal year (e.g., "2024")' })
+    @ApiProperty({ description: 'Fiscal year (e.g., "2081/82")' })
+    @IsString()
+    @Matches(/^\d{4}\/\d{2}$/, {
+        message: 'Year must be in YYYY/YY format (e.g., "2081/82")'
+    })
     @Column()
     year: string;
 
     @ApiProperty({ description: 'Start date of the fiscal year' })
+    @IsDate()
     @Column({ type: 'date' })
     startedOn: Date;
 
     @ApiProperty({ description: 'End date of the fiscal year', required: false })
+    @IsOptional()
+    @IsDate()
     @Column({ type: 'date', nullable: true })
     closedOn: Date;
 
--- src/vacancy/entities/vacancy.entity.ts
+++ src/vacancy/entities/vacancy.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsInt, Min, Max, IsUUID } from 'class-validator';
 import { FiscalYear } from '../../fiscal-year/entities/fiscal-year.entity';
 
 @Entity()
@@ -9,26 +10,34 @@ export class Vacancy {
     id: string;
 
     @ApiProperty({ description: 'Level of the position (1-12)' })
+    @IsInt()
+    @Min(1)
+    @Max(12)
     @Column()
     level: number;
 
     @ApiProperty({ description: 'Service department' })
+    @IsString()
     @Column()
     service: string;
 
     @ApiProperty({ description: 'Group category' })
+    @IsString()
     @Column()
     group: string;
 
     @ApiProperty({ description: 'Sub-group category' })
+    @IsString()
     @Column()
     subGroup: string;
 
     @ApiProperty({ description: 'Position title' })
+    @IsString()
     @Column()
     position: string;
 
     @ApiProperty({ description: 'Associated fiscal year', type: () => FiscalYear })
+    @IsUUID()
     @ManyToOne(() => FiscalYear, fiscalYear => fiscalYear.vacancies)
     @JoinColumn()
     fiscalYear: FiscalYear;
